9799a76e8db13c8f5228326f7f1e622c7c6cc043
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2025-01-03 17:45:35 +0700

    KernelCI: Common: add python3

--- Common/Dependency.sh
+++ Common/Dependency.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install -y build-essential clang bc bison libssl-dev libfl-dev libncurses5-dev libzstd-dev libtinfo5
-sudo apt-get install -y curl git ftp lftp wget libarchive-tools ccache python2 python2-dev
+sudo apt-get install -y curl git ftp lftp wget libarchive-tools ccache python2 python2-dev python3
 sudo apt-get install -y zip unzip tar gzip bzip2 rar unrar cpio
           
