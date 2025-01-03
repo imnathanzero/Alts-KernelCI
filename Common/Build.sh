43cdaed7526330a7b18635df7dd20766738f3d48
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2025-01-03 12:37:56 +0700

    KernelCI: Common: add clone clang and path configuration

--- Common/Build.sh
+++ Common/Build.sh
@@ -3,3 +3,7 @@
 # Clone Kernel
 git clone --recursive --depth=1 -j $(nproc) $REPO -b $BRANCH $KERNELNAME
 cd $KERNELNAME
+
+# Clone Clang
+git clone --recursive --depth=1 -j $(nproc) $CLANGREPO -b $CLANGBRANCH $CLANGNAME
+PATH="${PWD}/$CLANGNAME/bin:${PATH}"
