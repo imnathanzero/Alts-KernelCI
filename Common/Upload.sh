03ae5a513b3ba7cef81a3a0ad5dbf619afb944c3
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2025-01-03 19:01:10 +0700

    KernelCI: Fixup AnyKernel Dir

--- Common/Upload.sh
+++ Common/Upload.sh
@@ -13,7 +13,7 @@ tgs() {
 
 # Push kernel to channel
 push() {
-    cd AnyKernel || exit 1
+    cd AnyKernel3 || exit 1
     ZIP=$(echo *.zip)
     tgs "${ZIP}" "Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s). | For $DEVICENAME "
 }
