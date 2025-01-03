8ad0a7918266e1d15299a69244d15a20fa39612c
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2025-01-03 15:07:04 +0700

    KernelCI: Common: Add Build User & Host Name

--- Common/Build.sh
+++ Common/Build.sh
@@ -7,3 +7,14 @@ cd $KERNELNAME
 # Clone Clang
 git clone --recursive --depth=1 -j $(nproc) $CLANGREPO -b $CLANGBRANCH $CLANGNAME
 PATH="${PWD}/$CLANGNAME/bin:${PATH}"
+
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+DATE=$(date +"%Y%m%d-%H%M")
+START=$(date +"%s")
+KERNEL_DIR=$(pwd)
+ARCH=arm64
+export ARCH
+KBUILD_BUILD_HOST="$HOST"
+export KBUILD_BUILD_HOST
+KBUILD_BUILD_USER="$USER"
+export KBUILD_BUILD_USER
