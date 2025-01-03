7c57813363509e263a219615bb828e3f43056566
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2025-01-03 17:51:52 +0700

    KernelCI: Common: don't user build user/Host on input Workflow

--- Common/Build.sh
+++ Common/Build.sh
@@ -5,8 +5,8 @@ git clone --recursive --depth=1 -j $(nproc) $REPO -b $BRANCH $KERNELNAME
 cd $KERNELNAME
 
 # Clone Clang
-git clone --recursive --depth=1 -j $(nproc) $CLANGREPO -b $CLANGBRANCH $CLANGNAME
-PATH="${PWD}/$CLANGNAME/bin:${PATH}"
+git clone --recursive --depth=1 -j $(nproc) $CLANGREPO -b $CLANGBRANCH clang
+PATH="${PWD}/clang/bin:${PATH}"
 
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 DATE=$(date +"%Y%m%d-%H%M")
@@ -14,9 +14,9 @@ START=$(date +"%s")
 KERNEL_DIR=$(pwd)
 ARCH=arm64
 export ARCH
-KBUILD_BUILD_HOST="$HOST"
+KBUILD_BUILD_HOST="RenzAlt"
 export KBUILD_BUILD_HOST
-KBUILD_BUILD_USER="$USER"
+KBUILD_BUILD_USER="KernelCI"
 export KBUILD_BUILD_USER
 
 # Compile
