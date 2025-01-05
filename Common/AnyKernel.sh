800d04694562a82b4d138fa7dd770f82885119b7
Nathan Andrew <nathannxx@proton.me>
2025-01-06 04:25:22 +0700

    Update AnyKernel.sh: change anykernel repository

--- Common/AnyKernel.sh
+++ Common/AnyKernel.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Clone AnyKernel 
-git clone --recursive --depth=1 -j $(nproc) https://github.com/ALprjkt/Anykernel3 -b ysl AnyKernel3
+git clone --recursive --depth=1 -j $(nproc) https://github.com/Frostleaft07/Anykernel3 -b rmx1941 AnyKernel3
