3a29413c3c3b3881ed6c8897994052abcd840613
Nathan Andrew <nathannxx@proton.me>
2025-01-06 04:36:00 +0700

    Update post.sh: aaa

--- Common/post.sh
+++ Common/post.sh
@@ -3,21 +3,15 @@
 # Copyright 2024
 
 tgp() {
-    curl -sX POST https://api.telegram.org/bot"$TOKEN"/sendPhoto -d photo=$PHOTO -d chat_id="$CHAT_CH" -d parse_mode=Markdown -d disable_web_page_preview=true -d text="$1" &>/dev/null -d caption="#AltsProject #MI8953
-$KERNELNAME $DEVICENAME || Kernel 4.19
-Author: @RennAlt
+    curl -sX POST https://api.telegram.org/bot"$TOKEN"/sendPhoto -d photo=$PHOTO -d chat_id="$CHAT_CH" -d parse_mode=Markdown -d disable_web_page_preview=true -d text="$1" &>/dev/null -d caption="New Kernel Build For
+$KERNELNAME $DEVICENAME
+Author: @natehiggas00
 
 🔹[Changelog]($CHANGELOG)
 🔹[Download]($DOWNLOAD)
-🔹[Support Group](https://t.me/+DpywKc3nbn42OTE1)
 
 Notes: 
-
-Credits :
-• Thanks to bla for the help and reference
-• @bla for port msm8953
-• @bla For help me in adaptation 4.19
-• @bla @bla for help and tester"
+ea"
 }
 
 # Send Build Info
