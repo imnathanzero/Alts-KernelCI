f92c736ef712f81d6e465dd78fe1f7f6cb51de6a
Nathan Andrew <nathannxx@proton.me>
2025-01-10 08:29:36 +0700

    Update Dependency.sh

--- Common/Dependency.sh
+++ Common/Dependency.sh
@@ -5,3 +5,5 @@ sudo apt-get install -y build-essential clang bc bison libssl-dev libfl-dev libn
 sudo apt-get install -y curl git ftp lftp wget libarchive-tools ccache python2 python2-dev python3
 sudo apt-get install -y zip unzip tar gzip bzip2 rar unrar cpio
           
+git clone --depth=1 https://gitlab.com/LeCmnGend/clang.git -b clang-19 ~/TC/proton-clang
+git clone --depth=1 https://github.com/P-Salik/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-gnu-7.5.git -b master ~/TC/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-7.5
