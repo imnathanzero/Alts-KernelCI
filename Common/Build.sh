8ee15b3cfced2032c4869d2e512511391d968567
Nathan Andrew <nathannxx@proton.me>
2025-11-20 09:14:01 +0700

    fix build #4
    dumbass me

--- Common/Build.sh
+++ Common/Build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-mkdir mkdir -p $(pwd)/output
 
 # Clone Kernel
 git clone --recursive --depth=1 -j $(nproc) $REPO -b $BRANCH $KERNELNAME
@@ -23,53 +22,3 @@ export KBUILD_BUILD_HOST
 KBUILD_BUILD_USER="openthe3xit"
 export KBUILD_BUILD_USER
 export CROSS_COMPILE=$(pwd)/prebuilts/gcc/linux-x86/arm/arm-eabi-7.2/bin/arm-eabi-
-
-# Compile
-compile() {
-
-    if [ -d "out" ]; then
-        rm -rf $(pwd)/output && mkdir -p $(pwd)/output
-    fi
-
-    make -C $(pwd) O=output \
-    VARIANT_DEFCONFIG=msm8916_sec_j5lte_eur_defconfig \
-    SELINUX_DEFCONFIG=selinux_defconfig
-    make -C $(pwd) O=output
-
-    if ! [ -a "$IMAGE" ]; then
-        finderr
-        exit 1
-    fi
-
-    git clone --depth=1 https://github.com/imnathanzero/AnyKernel3.git AnyKernel -b j5
-    cp $(pwd)/output/arch/arm/boot/Image AnyKernel/zImage
-
-}
-
-# Zipping
-zipping() {
-    cd AnyKernel || exit 1
-    zip -r9 Alts-"${BRANCH}"-"${DEVICENAME}"-"${DATE}".zip ./*
-    cd ..
-}
-
-tgs() {
-    MD5=$(md5sum "$1" | cut -d' ' -f1)
-    curl -fsSL -X POST -F document=@"$1" https://api.telegram.org/bot"$TOKEN"/sendDocument \
-        -F chat_id="$CHAT_ID" \
-        -F "parse_mode=Markdown" \
-        -F "caption=$2 | *MD5*: \`$MD5\`"
-}
-
-# Push kernel to channel
-push() {
-    cd AnyKernel || exit 1
-    ZIP=$(echo *.zip)
-    tgs "${ZIP}" "Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s). | For $DEVICENAME "
-}
-
-compile
-zipping
-END=$(date +"%s")
-DIFF=$((END - START))
-push
--- Common/Zipping.sh
+++ Common/Zipping.sh
@@ -3,6 +3,9 @@
 DATE=$(date +"%Y%m%d-%H%M")
 START=$(date +"%s")
 
+git clone --depth=1 https://github.com/imnathanzero/AnyKernel3.git AnyKernel -b j5
+cp $(pwd)/output/arch/arm/boot/Image AnyKernel/zImage
+
 # Zipping
 zipping() {
     cd AnyKernel3 || exit 1
